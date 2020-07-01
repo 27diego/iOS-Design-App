9e5671546c46723b07c8ba5258f2d0db2a554d03
Developer <developer@Diegos-MacBook-Pro.local>
2020-07-01 13:32:28 -0700

    added horizonal scroll

--- designcode/Home.swift
+++ designcode/Home.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct Home: View {
     @State var showProfile = false
+    @State var viewState = CGSize.zero
     
     
     var body: some View {
@@ -17,40 +18,36 @@ struct Home: View {
             Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
                 .edgesIgnoringSafeArea(.all)
             
-            VStack {
-                HStack {
-                    Text("Watching")
-                        .font(.system(size: 28, weight: .bold))
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        self.showProfile.toggle()
-                    }) {
-                    Image("Avatar")
-                        .renderingMode(.original)
-                        .resizable()
-                        .frame(width: 36, height: 36)
-                        .clipShape(Circle())
-                    }
-                }
-                .padding(.horizontal)
-                .padding(.top, 30)
-                Spacer()
-            }
-            .padding(.top, 44)
-            .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
-            .offset(y: showProfile ? -450 : 0)
-            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 10.0, y: 0, z: 0))
-            .scaleEffect(showProfile ? 0.9 : 1)
-            .animation(.spring(response: 0.3, dampingFraction: 0.7, blendDuration: 0))
-            .edgesIgnoringSafeArea(.all)
+            HomeView(showProfile: $showProfile)
+                .padding(.top, 44)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+                .offset(y: showProfile ? -450 : 0)
+                .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10.0, y: 0, z: 0))
+                .scaleEffect(showProfile ? 0.9 : 1)
+                .animation(.spring(response: 0.3, dampingFraction: 0.7, blendDuration: 0))
+                .edgesIgnoringSafeArea(.all)
             
             MenuView()
-                .offset(y: showProfile ? 0 : 600)
+                .background(Color.black.opacity(0.001))
+                .offset(y: showProfile ? 0 : screen.height)
+                .offset(y: viewState.height)
                 .animation(.spring(response: 0.3, dampingFraction: 0.7, blendDuration: 0))
+                .onTapGesture {
+                    self.showProfile.toggle()
+                }
+                .gesture(
+                    DragGesture().onChanged{ value in
+                        self.viewState = value.translation
+                    }
+                    .onEnded { value in
+                        if self.viewState.height > 50 {
+                            self.showProfile = false
+                        }
+                    self.viewState = .zero
+                }
+            )
         }
     }
 }
@@ -60,3 +57,21 @@ struct Home_Previews: PreviewProvider {
         Home()
     }
 }
+
+struct AvatarView: View {
+    @Binding var showProfile: Bool
+    
+    var body: some View {
+        Button(action: {
+            self.showProfile.toggle()
+        }) {
+            Image("Avatar")
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 36, height: 36)
+                .clipShape(Circle())
+        }
+    }
+}
+
+let screen = UIScreen.main.bounds
