0ee154efbc14895464e7e9af4d977599c0d19e62
Developer <developer@Diegos-MacBook-Pro.local>
2020-07-04 12:54:53 -0700

    working with api data

---
+++ designcode/PostList.swift
@@ -0,0 +1,30 @@
+//
+//  PostList.swift
+//  designcode
+//
+//  Created by Developer on 7/4/20.
+//  Copyright © 2020 Diego Vega. All rights reserved.
+//
+
+import SwiftUI
+
+struct PostList: View {
+    @State var posts: [Post] = []
+    
+    var body: some View {
+        List(posts) { post in
+            Text(post.title)
+        }
+        .onAppear {
+            Api().getPosts { (posts) in
+                self.posts = posts
+            }
+        }
+    }
+}
+
+struct PostList_Previews: PreviewProvider {
+    static var previews: some View {
+        PostList()
+    }
+}
